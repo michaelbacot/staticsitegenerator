d5a4a1bbab1c357e8dbe4577ed88ffc0fce321f5
michaelbacot <mbacot77@gmail.com>
2026-07-25 14:14:36 -0400

    C: added textnode.py for using textnodes

--- public/src/main.py
+++ public/src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+
+    test_node = TextNode("This is some anchor text", TextType.LINK, "https://example.com")
+    print(test_node)
+
+if __name__ == "__main__":
+    main()
